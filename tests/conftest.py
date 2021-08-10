ec89a28e3930d89446c98f5241ed62345452e3db
sabotagebeats <27985126+sabotagebeats@users.noreply.github.com>
2021-08-10 20:08:00 +0000

    test: refactored testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,6 @@ def local_trezor_files():
     return glob.glob("./tests/trezor/*")
 
 
+@pytest.fixture
+def ape_trezor_vitalik():
+    return os.getenv("HOME") + "/.ape/trezor/vitalik.json"
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,9 +1,11 @@
 import os
 import glob
 
-def test_integration(local_trezor_files):
+
+def test_integration(local_trezor_files, ape_trezor_vitalik):
     assert "vitalik.json" in local_trezor_files[0]
-    os.system(f'cp {local_trezor_files[0]} {os.getenv("HOME")}/.ape/trezor/vitalik.json')
-    assert os.getenv("HOME") + "/.ape/trezor/vitalik.json" in glob.glob(os.getenv("HOME") + "/.ape/trezor/*")
-    os.system('ape trezor delete vitalik')
-    assert os.getenv("HOME") + "/.ape/trezor/vitalik.json" not in glob.glob(os.getenv("HOME") + "/.ape/trezor/*")
+    assert ape_trezor_vitalik not in glob.glob(os.getenv("HOME") + "/.ape/trezor/*")
+    os.system(f"cp {local_trezor_files[0]} {ape_trezor_vitalik}")
+    assert ape_trezor_vitalik in glob.glob(os.getenv("HOME") + "/.ape/trezor/*")
+    os.system("ape trezor delete vitalik")
+    assert ape_trezor_vitalik not in glob.glob(os.getenv("HOME") + "/.ape/trezor/*")
