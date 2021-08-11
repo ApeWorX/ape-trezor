7223f2678857858fec6ad31b427d4f5602e5ef60
sabotagebeats <27985126+sabotagebeats@users.noreply.github.com>
2021-08-11 22:05:54 +0000

    fix: type error not passing checks

--- ape_trezor/accounts.py
+++ ape_trezor/accounts.py
@@ -59,7 +59,7 @@ class TrezorAccount(AccountAPI):
         r = signature.signature[1:33]
         s = signature.signature[33:65]
         v = signature.signature[0]
-        return MessageSignature(v, r, s)
+        return MessageSignature(v, r, s)  # type: ignore
 
     def sign_transaction(self, txn: TransactionAPI) -> TransactionSignature:
         # NOTE: Some accounts may not offer signing things
@@ -75,4 +75,4 @@ class TrezorAccount(AccountAPI):
             txn.chain_id,
             # tx_type,
         )
-        return TransactionSignature(vrs[0], vrs[1], vrs[2])
+        return TransactionSignature(vrs[0], vrs[1], vrs[2])  # type: ignore
