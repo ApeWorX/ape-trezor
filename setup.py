c917895ecfa266b570b58ee9571158e086ea28d7
unparalleled-js <yingthi@live.com>
2023-01-30 11:12:50 -0600

    chore: add pyyaml types

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ extras_require = {
         "black>=22.12.0",  # auto-formatter and linter
         "mypy>=0.991",  # Static type analyzer
         "types-setuptools",  # Needed for mypy typeshed
+        "types-PyYAML",  # Needed for mypy typeshed
         "flake8>=5.0.4",  # Style linter
         "isort>=5.10.1",  # Import sorting linter
     ],
